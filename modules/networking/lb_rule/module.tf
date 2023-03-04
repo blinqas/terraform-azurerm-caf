e2a393cab730b0eca690c9f3665dc15701f230fe
Hein Tonny Køien <hein.koien@blinq.no>
2023-03-04 06:34:43 +0000

    Updated lb (load balancer) module, add possibility to refer backend pool and probes by key in rules

--- modules/networking/lb_rule/module.tf
+++ modules/networking/lb_rule/module.tf
@@ -10,18 +10,26 @@ resource "azurecaf_name" "lb" {
   use_slug      = try(var.settings.azurecaf_name.use_slug, var.global_settings.use_slug)
 }
 resource "azurerm_lb_rule" "lb" {
-  name                           = azurecaf_name.lb.result
+  name = azurecaf_name.lb.result
   # resource_group_name      = var.resource_group_name     // blinQ: Not supported argument with azurerm v3.29.1
   loadbalancer_id                = can(var.settings.loadbalancer.id) ? var.settings.loadbalancer.id : var.remote_objects.lb[try(var.settings.loadbalancer.lz_key, var.client_config.landingzone_key)][var.settings.loadbalancer.key].id
   frontend_ip_configuration_name = var.settings.frontend_ip_configuration_name
   protocol                       = var.settings.protocol
   frontend_port                  = var.settings.frontend_port
   backend_port                   = var.settings.backend_port
-  backend_address_pool_ids       = try(var.settings.backend_address_pool_ids, null)
-  probe_id                       = try(var.settings.probe_id, null)
+  backend_address_pool_ids       = try(local.backend_address_pool_ids, null)
+  probe_id                       = try(can(var.settings.probe.id) ? var.settings.probe.id : can(var.settings.probe_id) ? var.settings.probe_id : var.remote_objects.lb_probe[try(var.settings.probe.lz_key, var.client_config.landingzone_key)][try(var.settings.probe.key, var.settings.probe_key)].id, null)
   enable_floating_ip             = try(var.settings.enable_floating_ip, null)
   idle_timeout_in_minutes        = try(var.settings.idle_timeout_in_minutes, null)
   load_distribution              = try(var.settings.load_distribution, null)
   disable_outbound_snat          = try(var.settings.disable_outbound_snat, null)
   enable_tcp_reset               = try(var.settings.enable_tcp_reset, null)
+}
+
+locals {
+  backend_address_pool_ids = flatten(element([
+    for pool in keys(var.settings.backend_address_pools) : [
+      (var.remote_objects.lb_backend_address_pool[try(var.settings.backend_address_pools[var.settings.backend_address_pools.key][pool].lz_key, var.client_config.landingzone_key)][var.settings.backend_address_pools[pool].key].id)
+    ]
+  ],0))
 }
